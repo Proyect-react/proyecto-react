64b2a38782648d4d4b795d87c7286b89a90c324a
goldz9999 <adrianmiranda047@gmail.com>
2025-08-21 01:30:01 -0500

    css

--- src/Componentes/Inicio.js
+++ src/Componentes/Inicio.js
@@ -2,10 +2,22 @@ import React from "react";
 
 const Inicio = () => {
   return (
-    <div>
-      <h1>Bienvenido a la Página de Inicio</h1>
+    <div className="dashboard">
+      <header style={styles.header} className="dashboard-header">
+        <h1>Registro de Ventas</h1>
+      </header>
     </div>
   );
+};
+
+const styles = {
+  header: {
+    backgroundColor: "#ffffffff",
+    padding: "20px",
+    textAlign: "center",
+    borderRadius: "10px",
+    borderBottom: "1px solid #eee",
+  },
 };
 
 export default Inicio;
